923737cac13368345135dffe6e25efe6e7a0ccbd
abevitor <blueprates86@gmail.com>
2024-08-13 17:19:13 -0300

    projeto Tv(mudar algumas coisas)

---
+++ ProjetoSmartTv/src/SmartTv.java
@@ -0,0 +1,41 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("aumentando o volume para:  " + volume);
+        
+    }
+
+    public void abaixarVolume(){
+        volume--;
+        System.out.println("abaixando o volume para: " + volume);
+        
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+
+    }
+
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# java
-repositorio para armazenar todo conteudo do curso de java meu deus
