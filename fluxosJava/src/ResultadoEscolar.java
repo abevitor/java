9db59d2d41f8c4df3118ac9cc82c079cabccba6c
abevitor <blueprates86@gmail.com>
2024-08-20 15:44:35 -0300

    fluxos

---
+++ fluxosJava/src/ResultadoEscolar.java
@@ -0,0 +1,14 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 10;
+
+        if(nota>= 6){
+            System.out.println("aprovado");
+        }else {
+            System.out.println("reprovado");
+        }
+
+
+    }
+
+}
