ec3dd729425c52029f8a18faa3334ac2132ac2e0
abevitor <blueprates86@gmail.com>
2024-08-26 15:45:33 -0300

    processo

--- fluxosJava/src/ProcessoSeletivo.java
+++ fluxosJava/src/ProcessoSeletivo.java
@@ -1,7 +1,33 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     
     public static void main (String[] args) {
-        System.out.println("Processo seletivo");
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos(){
+        String[] candidatos = {"FELIPE","MARCIA","BILL","AUGUSTO","MONICA","MIQUELLA","DANIELA","JORGE","BEATRIZ","VITOR"};
+        int candidatosSelcionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelcionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido);
+            if(salarioBase >=salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga" );
+                candidatosSelcionados++;
+
+            }
+            candidatosAtual++;
+
+        }
+        
+    }
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1900, 2200);
     }
     static void analisarCandidato(double salarioPretendido){
         double salarioBase  = 2000.0;
